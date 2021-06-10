225944ea16091d3cf244d9286af36f793728f073
Dena Burd <denaburd@gmail.com>
2021-06-10 16:45:23 -0700

    Addds routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,33 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+from dotenv import load_dotenv
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+load_dotenv()
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    app.url_map.strict_slashes = False
+    from .routes import books_bp
+    app.register_blueprint(books_bp)
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    if test_config is None:
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI")
+    else:
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_TEST_DATABASE_URI")
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    from app.models.book import Book
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,73 @@
-from flask import Blueprint
+from app import db
+from app.models.book import Book
+from flask import request, Blueprint, make_response, jsonify
+books_bp = Blueprint("books", __name__, url_prefix="/books")
 
+
+@books_bp.route("/", methods=["GET", "POST"])
+def handle_books():
+
+    if request.method == "GET":
+        title_query = request.args.get("title")
+        if title_query:
+            books = Book.query.filter(Book.title.ilike(f'%{title_query}%'))
+        else:
+            books = Book.query.all()
+        books_response = []
+        for book in books:
+            books_response.append({
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            })
+        return make_response(jsonify(books_response), 200)
+
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_book = Book(title=request_body["title"],
+                        description=request_body["description"])
+
+        try:
+            db.session.add(new_book)
+            db.session.commit()
+            book_response = {"book": {
+                "id": new_book.id,
+                "title": new_book.title,
+                "description": new_book.description
+            }}
+            return make_response(book_response, 201)
+        except Exception as e:
+            print(e)
+            return make_response(f"Book was not created. Title and description are required.", 400)
+
+
+@ books_bp.route("/<book_id>", methods=["GET", "PUT", "DELETE"])
+def handle_book(book_id):
+    book = Book.query.get(book_id)
+    if not book:
+        return make_response(f"Book #{book_id} Not Found", 404)
+
+    if request.method == "GET":
+        book_response = {"book": {
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        }}
+        return make_response(book_response, 200)
+
+    elif request.method == "PUT":
+        form_data = request.get_json()
+
+        book.title = form_data["title"]
+        book.description = form_data["description"]
+        db.session.commit()
+        book_response = {"book": {
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        }}
+        return make_response(book_response, 200)
+    elif request.method == "DELETE":
+        db.session.delete(book)
+        db.session.commit()
+        return make_response(f"Book # {book.id} successfully deleted", 200)
